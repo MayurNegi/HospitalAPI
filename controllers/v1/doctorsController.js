c9232de3d01afedffbe26a1e06c03d0182fe76e6
Mayur <mayurnegi1999@gmail.com>
2020-07-13 20:38:20 +0530

    added login for doctor

--- controllers/v1/doctorsController.js
+++ controllers/v1/doctorsController.js
@@ -1,5 +1,6 @@
 constjwt = require("jsonwebtoken");
 const Doctor = require("../../models/doctor");
+const { ExtractJwt } = require("passport-jwt");
 
 module.exports.doctorRegistration = async function (req, res) {
   try {
@@ -28,6 +29,31 @@ module.exports.doctorRegistration = async function (req, res) {
     console.log("error", err);
     return res.status(500).json({
       message: "Internal Server error",
+    });
+  }
+};
+
+module.exports.login = async function (req, res) {
+  try {
+    let doctor = await Doctor.findOne({ email: req.body.email });
+    // if password is incorrect or user is not registered
+    if (!doctor || doctor.password != req.body.password) {
+      return res.status(422).json({
+        message: "invalid username or password",
+      });
+    }
+
+    // login the user
+    return res.status(200).json({
+      message: "login successfully",
+      data: {
+        token: jwt.sign(doctor.toJSON(), "secret", { expiresIn: 100000 }),
+      },
+    });
+  } catch (err) {
+    console.log("error", err);
+    return res.status(500).json({
+      message: "internal server error",
     });
   }
 };
